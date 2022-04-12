a5644946a1e6646440dba91679a18f3821b81b50
Lucas Krause <lucas.rksr@gmail.com>
2022-04-12 17:36:29 -0300

    Update CodeWars - Labirinto.py

--- CodeWars - Labirinto.py
+++ CodeWars - Labirinto.py
@@ -11,104 +11,78 @@ import queue
 import copy
 from time import sleep
 
-# O critério utilizado para saber se o labirinto é válido ou não foi:
-# Se o robô volta para o início, o labirinto é descartado.
-# Se o robô chega ao S, o labirinto é validado.
-
+# O critério utilizado para saber se o labirinto é válido ou não, foi: se o robô volta para o início, o labirinto é descartado; se o robô chega ao S, o labirinto é validado.
 verificador_de_labirinto = 1 
 # Dígito verificador para identificar se o labirinto tem solução ou não.
-
 while verificador_de_labirinto == 1: 
-
- # Função para gerar labirintos aleatóriamente. 
- def cria_labirinto():
+ 
+ def gerador_labirinto():
   labirinto = []
 
-  lista_espaco_cerquilha = [' ', '#']
+  espaco_cerquilha = [' ', '#']
 
   linha_zero = ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#']
   labirinto.append(linha_zero)
-  linha_um = [random.choice(lista_espaco_cerquilha) for x in range(17)]
-  linha_um.insert(0, '#')
-  linha_um.insert(18, '#')
-  inicio = random.randint(1, 7) # Limitação do 1 ao 7 para que o início fique mais distante do final do labirinto.
-  linha_um.insert(inicio, 'X') # Variavel início criada para guardar onde o robô vai iniciar o trajeto.
+  linha_um = [random.choice(espaco_cerquilha) for x in range(17)]
+  linha_um.insert(0, '#'), linha_um.insert(18, '#')
+  inicio = random.randint(1, 7) #Limitação do 1 ao 7 para que o início fique mais distante do final do labirinto.
+  linha_um.insert(inicio, 'X') #Variavel início criada para guardar onde o robô vai iniciar o trajeto.
   labirinto.append(linha_um) 
-  linha_dois = [random.choice(lista_espaco_cerquilha) for x in range(17)] 
-  linha_dois.insert(0, '#')
-  linha_dois.insert(19, '#')
-  linha_dois.insert(inicio, ' ') # Espaço criado para que o robô dê o primeiro passo e comece a validar as condições.
+  linha_dois = [random.choice(espaco_cerquilha) for x in range(17)] 
+  linha_dois.insert(0, '#'), linha_dois.insert(19, '#')
+  linha_dois.insert(inicio, ' ') #Espaço criado para que o robô dê o primeiro passo e comece a validar as condições.
   labirinto.append(linha_dois)
-  linha_tres = [random.choice(lista_espaco_cerquilha) for x in range(18)]
-  linha_tres.insert(0, '#')
-  linha_tres.insert(19, '#')
+  linha_tres = [random.choice(espaco_cerquilha) for x in range(18)]
+  linha_tres.insert(0, '#'), linha_tres.insert(19, '#')
   labirinto.append(linha_tres)
-  linha_quatro = [random.choice(lista_espaco_cerquilha) for x in range(18)]
-  linha_quatro.insert(0, '#')
-  linha_quatro.insert(19, '#')
+  linha_quatro = [random.choice(espaco_cerquilha) for x in range(18)]
+  linha_quatro.insert(0, '#'), linha_quatro.insert(19, '#')
   labirinto.append(linha_quatro)
-  linha_cinco = [random.choice(lista_espaco_cerquilha) for x in range(18)]
-  linha_cinco.insert(0, '#')
-  linha_cinco.insert(19, '#')
+  linha_cinco = [random.choice(espaco_cerquilha) for x in range(18)]
+  linha_cinco.insert(0, '#'), linha_cinco.insert(19, '#')
   labirinto.append(linha_cinco)
-  linha_seis = [random.choice(lista_espaco_cerquilha) for x in range(18)]
-  linha_seis.insert(0, '#')
-  linha_seis.insert(19, '#')
+  linha_seis = [random.choice(espaco_cerquilha) for x in range(18)]
+  linha_seis.insert(0, '#'), linha_seis.insert(19, '#')
   labirinto.append(linha_seis)
-  linha_sete = [random.choice(lista_espaco_cerquilha) for x in range(18)]
-  linha_sete.insert(0, '#')
-  linha_sete.insert(19, '#')
+  linha_sete = [random.choice(espaco_cerquilha) for x in range(18)]
+  linha_sete.insert(0, '#'), linha_sete.insert(19, '#')
   labirinto.append(linha_sete)
-  linha_oito = [random.choice(lista_espaco_cerquilha) for x in range(18)]
-  linha_oito.insert(0, '#')
-  linha_oito.insert(19, '#')
+  linha_oito = [random.choice(espaco_cerquilha) for x in range(18)]
+  linha_oito.insert(0, '#'), linha_oito.insert(19, '#')
   labirinto.append(linha_oito)
   linha_nove = ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#']
-  linha_nove.insert((random.randint(15, 18)), 'S') # Limitação do 15 ao 18 para que o final fique mais distante do início do labirinto.
+  linha_nove.insert((random.randint(15, 18)), 'S') #Limitação do 15 ao 18 para que o final fique mais distante do início do labirinto.
   labirinto.append(linha_nove)
    
   return labirinto
    
-
- labirinto = cria_labirinto()
+ labirinto = gerador_labirinto()
  labirinto_certo = copy.deepcopy(labirinto)
-  # copy.deepcopy(x[, memo]) Return a deep copy of x. (Documentação Oficial).
-  # Faz uma cópia fiel do mapa validado, sem as marcações do caminho do robô utilizadas no processo.
  
-  
-  # Regra de movimentação do robô
-
- # Posição inicial do robô
-  # x e y agora está sendo coletado direto do labirinto inicial. Se aleatorizarmos onde 'X' vai aparecer, automaticamente ele já vai encontrar e retornar x e y.
+#Processo de validação do labirinto utilizando o próprio robô para descartar mapas sem resolução. 
  for lista in labirinto:
    if 'X' in lista:
     x = lista.index('X')
     y = labirinto.index(lista)
 
  caminho_percorrido = queue.LifoQueue()
-
-  
- # Variável n fora do while para contabilizar todos os passos
+ 
  n = 0
-
- #Variáveis para armazenar o local inicial do robô.
  inicio_x = x
  inicio_y = y
 
-
  while True:
-# Definição das variáveis dentro do while pq precisam ser atualizadas a cada movimento
+   
     posicao_atual = labirinto[y][x]
     posicao_acima = labirinto[y-1][x]
     posicao_direita = labirinto[y][x+1]
     posicao_abaixo = labirinto[y+1][x]
     posicao_esquerda = labirinto[y][x-1]
-   
-  
-# Contador de passos.
+    
+#Contador de passos.
     n += 1
    
- # Movimento final/vitória
+#Movimento final/vitória
     if posicao_acima == 'S':
       labirinto[y][x] = '.'
       labirinto[y-1][x] = 'X'
@@ -138,15 +112,11 @@ while verificador_de_labirinto == 1:
       verificador_de_labirinto = 0
       break
      
-
     elif n != 1 and (x == inicio_x) and (y == inicio_y): 
-      
       break
        
-    
     else:
-  
- # Movimento padrão
+#Movimento padrão
       if posicao_acima == ' ':
         caminho_percorrido.put(tuple([x , y]))
         labirinto[y][x] = '.'
@@ -169,29 +139,24 @@ while verificador_de_labirinto == 1:
         x -= 1
       else:
 
-# Movimento beco sem saída
+#Movimento beco sem saída
         andar_pra_tras = caminho_percorrido.get()
         labirinto[y][x] = '.'
         x , y = andar_pra_tras
         labirinto[y][x] = 'X'
   
    
-# Cópia válida e sem movimentos, gerada com a válidação do robô.
 labirinto = labirinto_certo
 
-
 print('Este é o labirinto com a posição inicial do robô.')
-
 for lista in labirinto:
   for posicao in lista:
     print(posicao, end=' ')
   print()
 print('\n')
 
-# Regra de movimentação do robô
-
-# Posição inicial do robô
-# x e y agora está sendo coletado direto do labirinto inicial. Se aleatorizarmos onde 'X' vai aparecer, automaticamente ele já vai encontrar e retornar x e y.
+#Regra de movimentação do robô
+#Posição inicial do robô
 for lista in labirinto:
   if 'X' in lista:
     x = lista.index('X')
@@ -199,18 +164,18 @@ for lista in labirinto:
 
 caminho_percorrido = queue.LifoQueue()
 
-# Variável n fora do while para contabilizar todos os passos
+#Variável n fora do while para contabilizar todos os passos
 n = 0
 
 while True:
-# Definição das variáveis dentro do while pq precisam ser atualizadas a cada movimento
+#Definição das variáveis dentro do while pq precisam ser atualizadas a cada movimento
   posicao_atual = labirinto[y][x]
   posicao_acima = labirinto[y-1][x]
   posicao_direita = labirinto[y][x+1]
   posicao_abaixo = labirinto[y+1][x]
   posicao_esquerda = labirinto[y][x-1]
   
-  # Contador de passos
+#Contador de passos
   n += 1
   if n == 1:
     print(f'Foi dado {n} passo até aqui.')
@@ -218,7 +183,7 @@ while True:
     print(f'Foram dados {n} passos até aqui.')
 
 
- # Movimento final/vitória
+#Movimento final/vitória
   if posicao_acima == 'S':
     labirinto[y][x] = '.'
     labirinto[y-1][x] = 'X'
@@ -265,7 +230,7 @@ while True:
     break
   else:
   
- # Movimento padrão
+#Movimento padrão
     if posicao_acima == ' ':
       caminho_percorrido.put(tuple([x , y]))
       labirinto[y][x] = '.'
@@ -288,7 +253,7 @@ while True:
       x -= 1
     else:
 
-# Movimento beco sem saída
+#Movimento beco sem saída
       andar_pra_tras = caminho_percorrido.get()
       labirinto[y][x] = '.'
       x , y = andar_pra_tras
